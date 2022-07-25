8719b8d53bb7b534747dca6219658a3b304207db
Mia Borja <apk.borja@gmail.com>
2022-07-25 16:16:50 -0700

    updated addUser and login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { Book, User } = require('../models');
 
 const resolvers = {
@@ -10,12 +11,29 @@ const resolvers = {
     },
     Mutation: {
         // uses Auth token generated on addUser to login to engine
-        login: async () => {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
 
+            if (!user) {
+                throw new AuthenticationError('No user exists with this email.')
+            }
+
+            const chPw = await user.isCorrectPassword(password);
+
+            if (!chPw) {
+                throw new AuthenticationError('Password is incorrect.')
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
         },
         // creates User and generates Auth token
-        addUser: async (parent, { username, email }) => {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user)
 
+            return { token, user };
         },
         // saves Book to signed in User's page
         saveBook: async () => {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -44,3 +44,5 @@ const typeDefs = gql`
         removeBook(bookId: ID!): User
     }
 `
+
+module.exports = typeDefs;
