3719a71fa2044bd27decfc94eab8ee80e6713a3e
Mia Borja <apk.borja@gmail.com>
2022-07-25 02:57:32 -0700

    added skeleton for mutations.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,28 @@
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Number
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID
+        authors: []
+        description: String
+        title: String
+        image: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+    
+    }
+`
