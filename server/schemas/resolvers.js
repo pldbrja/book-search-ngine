c7e23023e44814a6c09adec6b14b4735fce41e1a
Mia Borja <apk.borja@gmail.com>
2022-07-25 02:55:21 -0700

    created mutations and queries utils for client-side

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,32 @@
-const { Thought } = require('../models');
+const { Book, User } = require('../models');
+
+const resolvers = {
+    Query: {
+        book: async () => {
+            return Book.find({});
+        },
+        me: async () => {
+            return User.find();
+        },
+    },
+    Mutation: {
+        // uses Auth token generated on addUser to login to engine
+        login: async () => {
+
+        },
+        // creates User and generates Auth token
+        addUser: async () => {
+
+        },
+        // saves Book to signed in User's page
+        saveBook: async () => {
+
+        },
+        // removes Book from User's page
+        removeBook: async () => {
+
+        },
+    }
+}
+
+module.exports = resolvers;
